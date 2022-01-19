4341e63ed0ddabc38a7fbfe1d926bc1b8edbfbc1
kyoheitsuno <freetrip555@gmail.com>
2022-01-19 16:29:56 +0900

    add test EC2 instance parameter
     - ami, instance_type, subnet_idを追加

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -3,5 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "test" {
-    ami     = "ami-032d6db78f84e8bf5"
+    ami             = "ami-032d6db78f84e8bf5"
+    instance_type   = "t3.micro"
+    subnet_id       = "subnet-06b7c9a0e8e05ae20"
 }
