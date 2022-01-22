930b3991afd955ab1aa54a8d16f6f238bf4222d3
kyoheitsuno <freetrip555@gmail.com>
2022-01-22 14:46:26 +0900

    fix resource aws_lb security_groups
     - セキュリティーグループの指定を構築した値に変更

--- terraform/env/dev/alb.tf
+++ terraform/env/dev/alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "sample_alb" {
   name               = "eks-sample-elb"
   internal           = false
   load_balancer_type = "application"
-  # security_groups    = [aws_security_group.lb_example.id]
+  security_groups    = [aws_security_group.lb_sample_sg.id]
   subnets = module.vpc.public_subnets
 }
 
